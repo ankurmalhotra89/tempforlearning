a3bd786b078849180b904f1872c9e03154c2d05d
ankurmalhotra89 <ankurmalhotra89@gmail.com>
2024-02-04 10:53:43 -0500

    test cases uysing pytest

--- test_add_numbers.py
+++ test_add_numbers.py
@@ -1,28 +1,15 @@
-import unittest
+# test_math_operations.py
+
 from ankurhello.math_operations import add_numbers
 
+def test_add_numbers_positive():
+    assert add_numbers(5, 3) == 8
 
-class TestAddNumbers(unittest.TestCase):
-    def test_add_numbers(self):
-        # Test case 1: Adding two positive numbers
-        result = add_numbers(5, 3)
-        self.assertEqual(result, 8)
+def test_add_numbers_negative():
+    assert add_numbers(-5, 3) == -2
 
-        # Test case 2: Adding a positive and a negative number
-        result = add_numbers(-5, 3)
-        self.assertEqual(result, -2)
+def test_add_numbers_floats():
+    assert add_numbers(2.5, 3.5) == 6.0
 
-        # Test case 3: Adding two negative numbers
-        result = add_numbers(-5, -3)
-        self.assertEqual(result, -8)
-
-        # Test case 4: Adding two floats
-        result = add_numbers(2.5, 3.5)
-        self.assertEqual(result, 6.0)
-
-        # Test case 5: Adding an integer and a float
-        result = add_numbers(2, 3.5)
-        self.assertEqual(result, 5.5)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_numbers_mixed():
+    assert add_numbers(2, 3.5) == 5.5
