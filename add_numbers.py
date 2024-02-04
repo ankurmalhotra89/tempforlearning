45d6d6161b89a48862f640c1dfaabbdcaab50d54
ankurmalhotra89 <ankurmalhotra89@gmail.com>
2024-02-04 10:48:10 -0500

    calculated added

---
+++ add_numbers.py
@@ -0,0 +1,6 @@
+from ankurhello.math_operations import add_numbers
+
+
+# Example usage:
+result = add_numbers(5, 13)
+print("Result:", result)  # Output will be 8
---
+++ ankurhello/math_operations.py
@@ -0,0 +1,14 @@
+# math_operations.py
+
+def add_numbers(a, b):
+    """
+    Adds two numbers together.
+
+    Args:
+    a (int, float): The first number.
+    b (int, float): The second number.
+
+    Returns:
+    int, float: The sum of the two numbers.
+    """
+    return a + b
